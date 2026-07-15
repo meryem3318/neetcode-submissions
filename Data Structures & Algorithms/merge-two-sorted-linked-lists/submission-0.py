ddab2f66e1ee7d23f625e694e28534e6e6b204f5
meryem3318 <m.choubaili1234@gmail.com>
2026-07-15 14:30:23 +0100

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+
+        # 2. Loop while both lists have nodes
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next  # Move tail forward!
+
+        # 3. Attach leftovers
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+
+        # 4. Return the head of the merged list
+        return dummy.next
